f96c708ab338ba8f69bc27d0337494c114e1d255
Ramy Aboul Naga <ramy.naga@gmail.com>
2017-03-03 18:24:38 +0200

    Fix default API endpoint

--- lib/london_cab/config.rb
+++ lib/london_cab/config.rb
@@ -12,7 +12,7 @@ module LondonCab
     attr_accessor(*Config::ATTRIBUTES)
 
     def reset
-      self.endpoint   = 'http://65.182.108.16:3467/api'
+      self.endpoint   = 'http://65.182.108.16:3467/api/CorporateAPI'
       self.user_agent = "LondonCab/#{LondonCab::VERSION}"
       self.token      = nil
       self.logger     = nil
